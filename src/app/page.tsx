ce6b54dc19b32a65978f6a28866ee7f4325904cc
Christian <christian.gulkoder@gmail.com>
2025-10-13 12:57:56 +0200

    Populære menyer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/Hero";
 import MøtKokken from "@/components/MøtKokken";
 import Services from "@/components/VåreTjenester";
+import ServicesWithImages from "@/components/PopulæreMenyer";
 
 export default function Home() {
   return <main>
     <HeroSection/>
     <MøtKokken/>
     <Services/>
+    <ServicesWithImages/>
     </main>;
 }
