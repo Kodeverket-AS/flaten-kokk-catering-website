d36c4f75650c63547ee7a9580be35b851397d5ab
https://github.com/Enforsix <christian.gulkoder@gmail.com>
2025-10-17 13:41:31 +0200

    hero alle

--- src/app/AirbnbEvents/page.tsx
+++ src/app/AirbnbEvents/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import React from "react";
+import HeroSection from "@/components/Hero";
+import { useRouter } from "next/navigation";
+
 export default function Page() {
-  return <main>Content</main>;
+  const router = useRouter();
+
+  return (
+    <main>
+      <HeroSection
+        title="Airbnb Events"
+        subtitle="Gjør ditt Airbnb-opphold ekstra spesielt med profesjonell mat og service"
+        buttonText="Kontakt meg"
+        onButtonClick={() => router.push("/Bestilling")}
+        backgroundImage="https://images.unsplash.com/photo-1503264116251-35a269479413?auto=format&fit=crop&w=1950&q=80"
+      />
+    </main>
+  );
 }
