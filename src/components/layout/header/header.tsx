458e26a81e83b6da8fa09cc7040502356bab71e3
https://github.com/Enforsix <christian.gulkoder@gmail.com>
2025-10-17 14:57:33 +0200

    mobil navbar

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -4,8 +4,8 @@ import { NavBarMobile } from "../navigation/mobile/navbar";
 export function Header() {
   return (
     <header className="">
-      <NavBarDesktop />
-      <NavBarMobile />
+      <div className="hidden lg:block"><NavBarDesktop /></div>
+      <div className="block lg:hidden"><NavBarMobile /></div>         
     </header>
   );
 }
