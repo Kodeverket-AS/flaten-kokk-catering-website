ab41cf5fc1565cc7574f2fd7ec10c61fdea7ca0b
stian <stiankarlsen709@gmail.com>
2025-11-05 14:23:19 +0100

    Enkelt å Bestille komponent gjort responsive

--- src/components/EnkeltÅBestille.tsx
+++ src/components/EnkeltÅBestille.tsx
@@ -18,14 +18,17 @@ const EnkelBestille: React.FC<ContainerProps> = ({
 }) => {
   return (
     <div className="bg-(--color-gray-100) w-full px-20 py-24">
-      <div className="w-full h-70 flex flex-col items-center">
+      <div className="w-full h-auto sm:h-70 flex flex-col items-center">
         <h2 className="font-playfair font-semibold text-black text-center mb-6">
           {title}
         </h2>
 
-        <div className="h-[80%] w-[75%] flex justify-around items-center">
+        <div className="flex flex-col gap-6 w-full h-auto items-stretch sm:h-[80%] sm:flex sm:flex-row sm:justify-around sm:items-center">
           {sections.map((Card) => (
-            <div key={Card.id} className="h-full flex flex-col items-center">
+            <div
+              key={Card.id}
+              className="h-full max-w-65 flex flex-col items-center"
+            >
               <div className="">
                 <div className="bg-(--color-amber-500) rounded-full m-auto pb-0.5 h-16 w-16 font-semibold text-white flex justify-center items-center">
                   {Card.number}
