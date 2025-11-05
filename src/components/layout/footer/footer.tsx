5ef31e81b85efd9fd13205545e30112284606700
Stein-B06 <Steinb06.kodehode@gmail.com>
2025-11-05 11:17:03 +0100

    added TW

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -59,13 +59,13 @@ export function Footer() {
           {/* Mobile version */}
           <div className="grid grid-cols-2 sm:grid-cols-3 gap-8">
             <div className="flex flex-col gap-3">
-              <h3 className="text-lg font-semibold">Hurtiglenker</h3>
+              <p className="title">Hurtiglenker</p>
               <ul className="text-base space-y-4 mb-3 ">
                 {links.map((link) => (
                   <li className="" key={link.label}>
                     <Link
                       href={link.path}
-                      className="inline links-hover cursor-pointer nodrag"
+                      className="inline sm:links-hover cursor-pointer nodrag active:text-[#f59e0b] focus:text-[#f59e0b]"
                     >
                       {link.label}
                     </Link>
@@ -75,11 +75,11 @@ export function Footer() {
             </div>
 
             <div className="flex flex-col gap-4">
-              <h3 className="text-lg font-semibold">Kontakt</h3>
+              <p className="title">Kontakt</p>
 
               <a
                 href="tel:+4712345678"
-                className="flex items-center gap-2 links-hover nodrag"
+                className="flex items-center gap-2 sm:links-hover active:text-[#f59e0b] focus:text-[#f59e0b] nodrag"
                 aria-label="Ring oss"
               >
                 <Phone className="w-5 h-5" />
@@ -88,7 +88,7 @@ export function Footer() {
 
               <a
                 href="mailto:"
-                className="flex items-center gap-2 links-hover nodrag"
+                className="flex items-center gap-2 sm:links-hover active:text-[#f59e0b] focus:text-[#f59e0b] nodrag"
                 aria-label="Send oss en epost"
               >
                 <Mail className="w-5 h-5" />
@@ -101,17 +101,14 @@ export function Footer() {
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label="Google maps"
-                  className="flex items-center gap-2 links-hover nodrag"
+                  className="flex items-center gap-2 sm:links-hover active:text-[#f59e0b] focus:text-[#f59e0b] nodrag"
                 >
                   <MapPin className="w-5 h-5" />
                   <span>Serverer hele Østlandet</span>
                 </a>
               </div>
-            </div>
-
-            <div className="w-full">
-              <div className="flex flex-col items-start w-full gap-2">
-                <h3>Åpningstider</h3>
+              <div className="mt-6 flex flex-col items-start w-full gap-2">
+                <p className="title">Åpningstider</p>
 
                 <div className="flex flex-col gap-2 items-start ">
                   <span>Man-Fre: 09:00-18:00</span>
@@ -133,7 +130,6 @@ export function Footer() {
                 height={100}
                 draggable={false}
               />
-              {/* <h3 className=" font-medium pb-2">Flaten Kokk og Catering</h3> */}
             </div>
             <p className="sm:max-w-sm md:max-w-sm xl:max-w-1xl">
               Profesjonell matlagning og catering-tjenester for alle
@@ -146,24 +142,24 @@ export function Footer() {
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Instagram"
-                className="text-pink-600 hover:text-pink-700 transition-colors nodrag"
+                className=" hover:text-pink-600 transition-colors nodrag"
               >
-                <FaInstagram className="w-8 h-8" />
+                <FaInstagram className="w-7 h-7" />
               </a>
               <a
                 href="#"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Facebook"
-                className="text-blue-600 hover:text-blue-700 transition-colors nodrag"
+                className=" hover:text-blue-600 transition-colors nodrag"
               >
-                <FaFacebook className="w-8 h-8" />
+                <FaFacebook className="w-7 h-7" />
               </a>
             </div>
           </div>
 
-          <div className="flex flex-col gap-4">
-            <h3 className="font-semibold">Hurtiglenker</h3>
+          <div className="flex flex-col gap-4 pb-4">
+            <p className="title">Hurtiglenker</p>
             <ul className="space-y-4">
               {links.map((link) => (
                 <li className="" key={link.label}>
@@ -179,7 +175,7 @@ export function Footer() {
           </div>
 
           <div className="flex flex-col gap-4">
-            <h3 className="text-lg font-semibold">Kontakt</h3>
+            <p className="title">Kontakt</p>
 
             <a
               href="tel:+4712345678"
@@ -213,7 +209,7 @@ export function Footer() {
             </div>
           </div>
           <div className="flex flex-col gap-3">
-            <h3>Åpningstider</h3>
+            <p className="title">Åpningstider</p>
 
             <div className="flex items-center gap-3">
               <Clock className="w-6 h-6" />
