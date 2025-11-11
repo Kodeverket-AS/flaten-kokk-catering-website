8f3e71669c3b30697ee532e4d5590ac11f909c50
Monika <maam092024@gmail.com>
2025-11-11 10:40:38 +0100

    layout

--- src/components/Galleri.tsx
+++ src/components/Galleri.tsx
@@ -10,18 +10,18 @@ const Galleri: React.FC<ImageGalleryProps> = ({ title, images }) => {
     <div className="wrapper-content">
       <h2 className="text-center">{title}</h2>
 
-      <div className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-6 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-1">
         {images.map((image, index) => (
           <div
             key={index}
-            className="group relative overflow-hidden rounded-xl shadow hover:shadow-lg transition-shadow duration-400 cursor-pointer"
+            className="group relative overflow-hidden shadow-md hover:shadow-2xl transition-shadow duration-500 cursor-pointer"
           >
             <img
               src={image.src}
               alt={image.alt}
-              className="w-full h-40 object-cover group-hover:scale-105 transition-transform duration-400"
+              className="w-full h-[320px]  lg:h-[300px] xl:h-[340px] object-cover transition-transform duration-500 group-hover:scale-[1.05]"
             />
-            <div className="absolute inset-0 bg-black/20 group-hover:bg-black/0 transition-all duration-400" />
+            <div className="pointer-events-none absolute inset-0 bg-black/20 group-hover:bg-black/0 transition-all duration-500" />
           </div>
         ))}
       </div>
