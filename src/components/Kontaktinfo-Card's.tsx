3dbe0863a4e588121376eac92d95b55567b86d05
stian <stiankarlsen709@gmail.com>
2025-11-18 13:31:34 +0100

    små fix på responsive design

--- src/components/Kontaktinfo-Card's.tsx
+++ src/components/Kontaktinfo-Card's.tsx
@@ -24,9 +24,9 @@ interface CombineProps extends KontaktinfoCardProps, BookingInfoProps {}
 
 function KontaktinfoCard(props: CombineProps) {
   return (
-    <div className="wrapper-content">
-      <div className="flex flex-col justify-center items-start h-full w-[20%]">
-        <div className="Kontakt-info h-fitt w-[308px] p-10 bg-stone-50 rounded-2xl border border-gray-200">
+    <div className="">
+      <div className="flex flex-col justify-center items-start gap-4">
+        <div className="Kontakt-info w-[308px] p-10 bg-stone-50 rounded-2xl border border-gray-200">
           <p className="title pb-6">Kontaktinformasjon</p>
 
           <div className="flex justify-start items-center gap-2 pb-2">
@@ -58,15 +58,30 @@ function KontaktinfoCard(props: CombineProps) {
 
         <div className="Booking-info h-fitt w-[308px] p-10 bg-stone-50 rounded-2xl border border-gray-200">
           <p className="title pb-6">Booking info</p>
-          <Clock size={24} />
-          <p className="font-medium pb-0.5">{props.varsel}</p>
-          <p className="text-xs font-neutral-900">{props.varselInfo}</p>
-          <Users size={24} />
-          <p className="font-medium pb-0.5">{props.personer}</p>
-          <p className="text-xs font-neutral-900">{props.personerInfo}</p>
-          <CreditCard size={24} />
-          <p className="font-medium pb-0.5">{props.forskudd}</p>
-          <p className="text-xs font-neutral-900">{props.forskuddInfo}</p>
+
+          <div className="flex justify-start items-center gap-2 pb-2">
+            <Clock size={24} />
+            <div className="">
+              <p className="font-medium pb-0.5">{props.varsel}</p>
+              <p className="text-xs font-neutral-900">{props.varselInfo}</p>
+            </div>
+          </div>
+
+          <div className="flex justify-start items-center gap-2 pb-2">
+            <Users size={24} />
+            <div className="">
+              <p className="font-medium pb-0.5">{props.personer}</p>
+              <p className="text-xs font-neutral-900">{props.personerInfo}</p>
+            </div>
+          </div>
+
+          <div className="flex justify-start items-center gap-2">
+            <CreditCard size={24} />
+            <div className="">
+              <p className="font-medium pb-0.5">{props.forskudd}</p>
+              <p className="text-xs font-neutral-900">{props.forskuddInfo}</p>
+            </div>
+          </div>
         </div>
 
         <div className="Foretrekker-tlf h-fitt w-[308px] p-10 bg-stone-50 rounded-2xl border border-gray-200 flex flex-col justify-center items-center">
@@ -74,18 +89,12 @@ function KontaktinfoCard(props: CombineProps) {
           <p className="pb-4 text-center">
             Ring meg for personlig rådgiving og skreddersydde løsninger
           </p>
-          {/* width: 228;
-height: 48;
-angle: 0 deg;
-opacity: 1;
-padding-top: 12px;
-padding-right: 24px;
-padding-bottom: 12px;
-padding-left: 24px;
-gap: 8px;
-border-radius: 8px; */}
 
-          <CallButton lable="Ring nå" phoneNumber="12345678" className="h-12 w-57 gap-2 cursor-pointer bg-amber-500 flex justify-center items-center rounded-2xl"/>
+          <CallButton
+            lable="Ring nå"
+            phoneNumber="12345678"
+            className="h-12 w-57 gap-2 cursor-pointer bg-amber-500 hover:bg-amber-700 flex justify-center items-center rounded-2xl"
+          />
         </div>
       </div>
     </div>
