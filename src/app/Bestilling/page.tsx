cee1bc87a99086288d839178b00b0287d2827781
Monika <maam092024@gmail.com>
2025-11-26 18:08:28 +0100

    Revert "Merge branch '95-bestiling-page--online-booking' into contact-form"
    This reverts commit 1498d378c9d7e2bdb827779f3a02f4245e15982c, reversing
    changes made to 53809c3fb9612996317484bf3ceec5e8d0bd76fa.

--- src/app/Bestilling/page.tsx
+++ src/app/Bestilling/page.tsx
@@ -5,8 +5,6 @@ import HeroSection from "@/components/Hero";
 import { useRouter } from "next/navigation";
 import Serviceområde from "@/components/Serviceområde";
 import ContactForm from "@/components/ContactForm";
-import KontaktInfoCard from "@/components/KontaktInfoCard";
-import KontaktSkjema from "@/components/KontaktInformasjon";
 
 export default function Page() {
   const router = useRouter();
@@ -22,37 +20,6 @@ export default function Page() {
       />
   
       <Serviceområde /> <ContactForm />
-      <div className=" wrapper-content">
-        <div className="w-full  mx-auto rounded-xl gap-4 flex flex-col md:flex-row pb-16 p-8">
-          <div className="flex-1">
-            <KontaktSkjema
-              title="Bestill catering"
-              submitButtonText="Send bestilling"
-              onSubmit={async (data) => {
-                console.log("Skjema data:", data);
-                // Her kan du legge til API-kall til Sanity eller annen backend
-              }}
-            />
-          </div>
-
-          <div className="kontakt-card shrink-0 mx-auto">
-            <KontaktInfoCard
-              phone="+47 123 45 678"
-              phoneInfo="Hverdager 09:00-17:00"
-              email="post@flatenkokk.no"
-              mailInfo="Svarer innen 24 timer"
-              location="Serverer hele Østlandet"
-              locationInfo="Reisekostnader beregnes"
-              varsel="Minimum 7 dager varsel"
-              varselInfo="For store arrangementer"
-              personer="Minimum 6 personer"
-              personerInfo="For private kokkoppdrag"
-              forskudd="50% forskudd"
-              forskuddInfo="Rest betales på dagen"
-            />
-          </div>
-        </div>
-      </div>
     </main>
   );
 }
