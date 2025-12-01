2f14491ddecb698ab4f312cc89b4f0e376e0f3fe
stian <stiankarlsen709@gmail.com>
2025-12-01 14:22:56 +0100

    safe save

--- src/components/KontaktInformasjon.tsx
+++ src/components/KontaktInformasjon.tsx
@@ -134,7 +134,6 @@ const KontaktSkjema: React.FC = () => {
         aria-labelledby="form-label"
       >
         <div className="h-full w-full px-10">
-          
           <OnlineBooking />
           <div className="h-px w-full bg-gray-400"></div>
           {/*-----------------------------Honneypot----------------------------------*/}
