464322e4f0bf56a9d0bd33e3335adf1f5cec16b0
Monika <maam092024@gmail.com>
2025-12-09 09:09:21 +0100

    corretion loading

--- src/app/Bestilling/page.tsx
+++ src/app/Bestilling/page.tsx
@@ -8,16 +8,6 @@ import ContactForm from "@/components/ContactForm";
 
 const Serviceområde = dynamic(() => import("@/components/Serviceområde"), {
   ssr: false,
-  loading: () => (
-    <div className="wrapper-content flex flex-col gap-6">
-      <div className="flex lg:flex-col-reverse items-center justify-center gap-4">
-        <div className="flex items-center">
-          <h2 className="text-center" style={{ lineHeight: '1', paddingBottom: '0' }}>Serviceområde</h2>
-        </div>
-      </div>
-      <p className="text-center">Vennligst vent, kartet lastes...</p>
-    </div>
-  ),
 });
 
 export default function Page() {
